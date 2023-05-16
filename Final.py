d61a68d951ee62c64732a28c1714934bf6e7f6d0
mitsuogui <130498334+mitsuogui@users.noreply.github.com>
2023-05-16 08:04:40 -0600

    Add files via upload

--- Final.py
+++ Final.py
@@ -5,6 +5,10 @@ import speech_recognition as sr
 
 class ParticipantsWindow(QWidget):
     def __init__(self):
+        """
+        This is a constructor function for a GUI window that allows users to add and remove participants
+        from a list.
+        """
         super().__init__()
         self.setWindowTitle('Registro de Participantes')
         self.layout = QVBoxLayout()
@@ -26,25 +30,34 @@ class ParticipantsWindow(QWidget):
         self.setLayout(self.layout)
 
     def add_participant(self):
+        """
+        This function adds a participant to a list and clears the input line.
+        """
         participant = self.input_line.text()
         if participant:
             self.participants_list.addItem(participant)
             self.input_line.clear()
 
     def remove_participant(self):
+        """
+        This function removes a selected participant from a list of participants.
+        """
         selected_item = self.participants_list.currentItem()
         if selected_item:
             self.participants_list.takeItem(self.participants_list.row(selected_item))
 
-
 class AgendaWindow(QWidget):
     def __init__(self):
+        """
+        This is a constructor function that sets up a GUI for a program that allows users to add, edit,
+        and remove items from a list.
+        """
         super().__init__()
         self.setWindowTitle('Registro de Agenda')
         self.layout = QVBoxLayout()
 
-        self.agenda_text = QTextEdit()
-        self.layout.addWidget(self.agenda_text)
+        self.apartados_list = QListWidget()
+        self.layout.addWidget(self.apartados_list)
 
         self.apartado_input = QLineEdit()
         self.layout.addWidget(self.apartado_input)
@@ -52,6 +65,9 @@ class AgendaWindow(QWidget):
         self.add_apartado_button = QPushButton('Agregar Apartado')
         self.add_apartado_button.clicked.connect(self.add_apartado)
         self.layout.addWidget(self.add_apartado_button)
+
+        self.puntos_list = QListWidget()
+        self.layout.addWidget(self.puntos_list)
 
         self.punto_input = QLineEdit()
         self.layout.addWidget(self.punto_input)
@@ -70,55 +86,50 @@ class AgendaWindow(QWidget):
 
         self.setLayout(self.layout)
 
-        self.agenda_data = {}
-
     def add_apartado(self):
+        """
+        This function adds an inputted "apartado" to a list and clears the input field.
+        """
         apartado = self.apartado_input.text()
         if apartado:
-            self.agenda_data[apartado] = []
-            self.update_agenda_text()
+            self.apartados_list.addItem(apartado)
             self.apartado_input.clear()
 
     def add_punto(self):
-        apartado, ok = QInputDialog.getItem(self, "Seleccionar Apartado", "Apartado:", self.agenda_data.keys(), editable=False)
+        """
+        This function adds a new item to a list widget based on user input.
+        """
+        apartado, ok = QInputDialog.getItem(self, "Seleccionar Apartado", "Apartado:", [self.apartados_list.item(i).text() for i in range(self.apartados_list.count())], editable=False)
         if ok and apartado:
             punto = self.punto_input.text()
             if punto:
-                self.agenda_data[apartado].append(punto)
-                self.update_agenda_text()
+                self.puntos_list.addItem(punto)
                 self.punto_input.clear()
 
     def edit_punto(self):
-        apartado, ok = QInputDialog.getItem(self, "Seleccionar Apartado", "Apartado:", self.agenda_data.keys(), editable=False)
-        if ok and apartado:
-            puntos = self.agenda_data[apartado]
-            punto, ok = QInputDialog.getItem(self, "Seleccionar Punto", "Punto:", puntos, editable=False)
-            if ok and punto:
-                nuevo_punto, ok = QInputDialog.getText(self, "Editar Punto", "Nuevo nombre del punto:", text=punto)
-                if ok and nuevo_punto:
-                    index = puntos.index(punto)
-                    self.agenda_data[apartado][index] = nuevo_punto
-                    self.update_agenda_text()
+        """
+        This function allows the user to edit the name of a selected item in a list of points.
+        """
+        selected_item = self.puntos_list.currentItem()
+        if selected_item:
+            nuevo_punto, ok = QInputDialog.getText(self, "Editar Punto", "Nuevo nombre del punto:", text=selected_item.text())
+            if ok and nuevo_punto:
+                selected_item.setText(nuevo_punto)
 
     def remove_punto(self):
-        apartado, ok = QInputDialog.getItem(self, "Seleccionar Apartado", "Apartado:", self.agenda_data.keys(), editable=False)
-        if ok and apartado:
-            puntos = self.agenda_data[apartado]
-            punto, ok = QInputDialog.getItem(self, "Seleccionar Punto", "Punto:", puntos, editable=False)
-            if ok and punto:
-                index = puntos.index(punto)
-                self.agenda_data[apartado].pop(index)
-                self.update_agenda_text()
-
-    def update_agenda_text(self):
-        self.agenda_text.clear()
-        for apartado, puntos in self.agenda_data.items():
-            self.agenda_text.append(f"{apartado}:")
-            for punto in puntos:
-                self.agenda_text.append(f"  - {punto}")
+        """
+        This function removes the selected item from a list widget.
+        """
+        selected_item = self.puntos_list.currentItem()
+        if selected_item:
+            self.puntos_list.takeItem(self.puntos_list.row(selected_item))
 
 class MainWindow(QMainWindow):
     def __init__(self):
+        """
+        This function initializes a main window for a registration system and sets the central widget to
+        a participants window while also creating a menu.
+        """
         super().__init__()
         self.setWindowTitle('Sistema de Registro')
 
@@ -130,6 +141,10 @@ class MainWindow(QMainWindow):
         self.create_menu()
 
     def create_menu(self):
+        """
+        This function creates a menu bar with two options, "Registro de Participantes" and "Registro de
+        Agenda", which when clicked, trigger the display of corresponding windows.
+        """
         menu_bar = self.menuBar()
         archivo_menu = menu_bar.addMenu('Archivo')
 
@@ -140,14 +155,24 @@ class MainWindow(QMainWindow):
         agenda_action.triggered.connect(self.show_agenda_window)
 
     def show_participants_window(self):
+        """
+        This function sets the central widget of the window to the participants window.
+        """
         self.setCentralWidget(self.participants_window)
 
     def show_agenda_window(self):
+        """
+        This function sets the central widget of the window to the agenda window.
+        """
         self.setCentralWidget(self.agenda_window)
 
 
 class AudioWindow(QWidget):
     def __init__(self):
+        """
+        This is a constructor function that sets up a GUI with buttons to select an audio file and
+        folder, and a button to recognize text from the audio file.
+        """
         super().__init__()
         self.setWindowTitle('División y Reconocimiento de Audio')
         self.layout = QVBoxLayout()
@@ -180,16 +205,27 @@ class AudioWindow(QWidget):
         self.recognize_button.clicked.connect(self.recognize_text)
 
     def select_audio_file(self):
+        """
+        This function opens a file dialog to select an audio file and sets the selected file path as the
+        text of a QLabel widget.
+        """
         file, _ = QFileDialog.getOpenFileName(self, 'Seleccionar archivo de audio')
         if file:
             self.audio_file_text.setText(file)
 
     def select_segments_folder(self):
+        """
+        This function opens a file dialog to select a folder and sets the selected folder path as text
+        in a QLineEdit widget.
+        """
         folder = QFileDialog.getExistingDirectory(self, 'Seleccionar carpeta para segmentos de grabación')
         if folder:
             self.segments_folder_text.setText(folder)
 
     def recognize_text(self):
+        """
+        The function takes in two text inputs and assigns them to variables.
+        """
         audio_file = self.audio_file_text.toPlainText()
         segments_folder = self.segments_folder_text.toPlainText()
 
@@ -211,14 +247,7 @@ class AudioWindow(QWidget):
                 text += f'Segmento {i}: {segment_text}\n'
 
             self.recognize_result.setText(text)
-    def choose_participant_from_agenda(agenda_data):
-        apartados = list(agenda_data.keys())
-        apartado, ok = QInputDialog.getItem(None, "Seleccionar Participante", "Apartado:", apartados, editable=False)
-        if ok and apartado:
-            QMessageBox.information(None, "Participante seleccionado", f"Has seleccionado al participante: {apartado}")
-            return apartado
-        else:
-            return None
+
 
 app = QApplication([])
 main_window = MainWindow()
